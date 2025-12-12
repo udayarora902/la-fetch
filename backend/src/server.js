449c2390cc5fad70032cf52aa7b5bc217abf44fd
udayarora902 <udayy1013@gmail.com>
2025-12-12 23:54:07 +0530

    finalize server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,33 @@
 import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
+
 import connectDb from "../lib/db.js";
+
+import userRoutes from "./routes/user.routes.js";
+import taskRoutes from "./routes/task.routes.js";
+
+import { errorHandler } from "./middleware/error.middleware.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// Built in middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: true, credentials: true }));
+
+// API routes
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
+
+// Global error handler
+app.use(errorHandler);
+
 app.listen(PORT, () => {
-  console.log("Server started on port", PORT);
+  console.log("Server running on port", PORT);
   connectDb();
 });
