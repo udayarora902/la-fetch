af477e5a323077b5a601a8334fddf0356cfc61e0
udayarora902 <udayy1013@gmail.com>
2025-12-12 22:37:18 +0530

    Add error handler middleware

---
+++ backend/src/middleware/error.middleware.js
@@ -0,0 +1,11 @@
+export const errorHandler = (err, req, res, next) => {
+  console.log("Error:", err);
+
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
+};
