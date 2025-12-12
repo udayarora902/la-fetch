190b298089b4a3a13a1437d70fc5e0e3fe7a9ac0
udayarora902 <udayy1013@gmail.com>
2025-12-12 23:15:29 +0530

    Add task controller

---
+++ backend/src/controllers/task.controller.js
@@ -0,0 +1,101 @@
+import Task from "../models/task.model.js";
+
+// Create task (admin only)
+export const createTask = async (req, res, next) => {
+  try {
+    const { title, description, assigned_to } = req.body;
+
+    if (!title || !description || !assigned_to) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const task = await Task.create({
+      title,
+      description,
+      assigned_to,
+    });
+
+    res.status(201).json({
+      message: "Task created",
+      task,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get all tasks (admin and user)
+export const getTasks = async (req, res, next) => {
+  try {
+    const tasks = await Task.find().populate("assigned_to", "name email role");
+    res.json(tasks);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Get single task
+export const getTaskById = async (req, res, next) => {
+  try {
+    const task = await Task.findById(req.params.id).populate(
+      "assigned_to",
+      "name email role"
+    );
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    res.json(task);
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Update task (admin OR assigned user)
+export const updateTask = async (req, res, next) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    // user role can only update own tasks
+    if (req.user.role === "user") {
+      if (task.assigned_to.toString() !== req.user._id.toString()) {
+        return res
+          .status(403)
+          .json({ message: "Not authorized to update this task" });
+      }
+    }
+
+    const updated = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.json({
+      message: "Task updated",
+      task: updated,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+// Delete task (admin only)
+export const deleteTask = async (req, res, next) => {
+  try {
+    const task = await Task.findById(req.params.id);
+
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+
+    await task.deleteOne();
+
+    res.json({ message: "Task deleted successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
